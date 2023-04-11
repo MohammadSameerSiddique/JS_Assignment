3cacccafa3da61116cd9e63c1f86e4a5e56f9269
MohammadSameerSiddique <siddiquesameer483@gmail.com>
2023-04-12 03:49:11 +0500

    work accomplish

--- Assignment-02/app.js
+++ Assignment-02/app.js
@@ -2,6 +2,7 @@
 // a specific number to any number passed to it. For example, if the closure is
 // created with 5, the returned function should add 5 to any number passed
 // to it.
+
 // function closure(num1) {
 //     return (num2) => {
 //         console.log(num1 + num2)
@@ -9,11 +10,14 @@
 // }
 // let add = closure(5);
 // let add2 = closure(15);
-// add(10)
-// add2(5)
+// add(10);
+// add2(5);
+
+
 // 2. Write a recursive function that searches an array for a specific value. The
 // function should return true if the value is found, and false if it is not. You
 // can assume that the array is not nested.
+
 // let arr = [2, 5, 6, 8, 12, 15];
 // let findArr = arr.filter(function (val) {
 //   if (val >= 3) {
@@ -23,18 +27,24 @@
 //   }
 // });
 // console.log(findArr);
+
+
 // 3. Write a function that adds a new paragraph element to the bottom of an
 // HTML document. The function should take a string argument that will be
 // used as the text content of the new paragraph element.
+
 // function paragraph (text) {
 //     let para = document.createElement("p");
 //     para.innerText = text;
 //     document.body.appendChild(para);
 // };
 // paragraph("A new paragraph element to the bottom of an HTML document");
+
+
 // 4. Write a function that adds a new list item to an unordered list in an HTML
 // document. The function should take a string argument that will be used as
 // the text content of the new list item.
+
 // function createlist (text) {
 //     let list = document.createElement("li");
 //     list.innerText = text;
@@ -43,10 +53,13 @@
 // };
 // createlist("Learn Javascript");
 // createlist("Learn React comming soon....");
+
+
 // 5. Write a function that changes the background color of an HTML element.
 // The function should take two arguments: the first argument is a reference
 // to the HTML element, and the second argument is a string representing
 // the new background color.
+
 // function bgcolor (theme) {
 //     let color = document.getElementById("style");
 //     color.style.backgroundColor = theme;
@@ -54,9 +67,12 @@
 // bgcolor('red');
 // bgcolor('green');
 // bgcolor('blue');
+
+
 // 6. Write a function that saves an object to localStorage. The function should
 // take two arguments: the first argument is a string representing the key to
 // use for storing the object, and the second argument is the object to store.
+
 // function obj () {
 //     let object = {
 //         name : "Sameer",
@@ -68,9 +84,12 @@
 //     localStorage.setItem("object", convertString);
 // }
 // obj();
+
+
 // 7. Write a function that retrieves an object from localStorage. The function
 // should take one argument, which is a string representing the key used to
 // store the object. The function should return the object.
+
 // function obj() {
 //   let object = {
 //     name: "Sameer",
